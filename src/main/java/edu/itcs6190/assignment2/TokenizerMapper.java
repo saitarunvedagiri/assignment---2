edcd519ab68f1cf603acb2c17fcdd1af2d108970
saitarunvedagiri <svedagi1@charlotte.edu>
2025-09-23 20:38:44 -0400

    Add files via upload

---
+++ src/main/java/edu/itcs6190/assignment2/TokenizerMapper.java
@@ -0,0 +1,22 @@
+package edu.itcs6190.assignment2;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>{
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken().replaceAll("[^a-zA-Z0-9]", "").toLowerCase());
+            if (!word.toString().isEmpty()) {
+                context.write(word, one);
+            }
+        }
+    }
+}
